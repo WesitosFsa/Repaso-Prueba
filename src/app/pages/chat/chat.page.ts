f30e7afb487e8d148c2a56a6a095418ca8a31819
WesitosFsa <137083225+WesitosFsa@users.noreply.github.com>
2024-11-17 22:12:45 -0500

    Servicios Chat y Login

--- src/app/pages/chat/chat.page.ts
+++ src/app/pages/chat/chat.page.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
+import { Observable, of } from 'rxjs'; // Usamos `of` para inicializar mensajes
+import { ChatService } from '../../services/chat.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.page.html',
-  styleUrls: ['./chat.page.scss'],
+selector: 'app-chat',
+templateUrl: './chat.page.html',
+styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
+  @ViewChild(IonContent) content: IonContent | undefined = undefined; // Inicialización a undefined
+  messages: Observable<any[]> = of([]); // Inicialización con un array vacío
+  newMsg = '';
 
-  constructor() { }
+  constructor(private chatService: ChatService, private router: Router) { }
 
-  ngOnInit() {
+ngOnInit() {
+    this.messages = this.chatService.getChatMessages();
+}
+
+  sendMessage() {
+    if (this.newMsg.trim() !== '') {
+      this.chatService.addChatMessage(this.newMsg).then(() => {
+        this.newMsg = '';
+        if (this.content) {
+          this.content.scrollToBottom(300);
+        }
+      });
+    }
   }
 
+  signOut() {
+    this.chatService.signOut().then(() => {
+      this.router.navigateByUrl('/', { replaceUrl: true });
+    });
+  }
 }
